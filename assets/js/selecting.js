757d2692e947406c29d09f5d6169e428801c1606
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-05-19 10:22:21 -0500

    Update selecting.js

--- assets/js/selecting.js
+++ assets/js/selecting.js
@@ -64,7 +64,7 @@ function redirect() {
     var waysToRoot = canRootBoard(document.getElementById("board").value);
     console.log(waysToRoot);
     if (waysToRoot.includes("SH1MMER") && document.getElementById("curVersion").value >= 112) {
-      window.location.href="https://chromebook-guide.github.io/sh1mmer-identify-wp";
+      window.location.href="https://chromebook-guide.github.io/identify-wp";
     } else if (document.getElementById("curVersion").value >= 112){
       alert("No exploits are available for your platform, since you're updated past v112. A workaround will be released by MercuryWorkshop in the coming weeks. Check back soon :)");
       return;
